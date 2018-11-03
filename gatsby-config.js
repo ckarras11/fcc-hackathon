9eed77d22a7d8720c8c6c255f55c4921ae333ec0
Christopher Karras <ckarras11@gmail.com>
2018-11-03 10:26:17 -0400

    Added favicon and logo, changed site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'SpareChange',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -16,13 +16,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'SpareChange',
+        short_name: 'SpareChange',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/SpareChange-icon.png', // This path is relative to the root of the site.
       },
     },
     {
