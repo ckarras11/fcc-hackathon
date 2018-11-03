c604c8f4ef64d07a7aec18be4bcf59667b2d7fe8
Christopher Karras <ckarras11@gmail.com>
2018-11-03 13:38:47 -0400

    Added Expense Page

---
+++ src/pages/Expenses.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const Expenses = () => (
+  <Layout>
+    <h1>Expenses</h1>
+    <p>Welcome to expenses!</p>
+  </Layout>
+)
+
+export default Expenses
