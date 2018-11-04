4b527453abcee044aec22ff174b9abf93e1b24e3
Christopher Karras <ckarras11@gmail.com>
2018-11-03 23:35:42 -0400

    Started adding content for expenses

--- src/pages/Expenses.js
+++ src/pages/Expenses.js
@@ -1,11 +1,34 @@
 import React from 'react'
-
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import Button from '@material-ui/core/Button'
+import AddIcon from '@material-ui/icons/Add'
 import Layout from '../components/layout'
+import Modal from '../components/modal'
+
+let data = [
+  { id: 1, kind: 'Dining', value: 100, color: '#aaac84' },
+  { id: 2, kind: 'Food', value: 200, color: '#dce7c5' },
+  { id: 3, kind: 'Gas', value: 50, color: '#e3a51a' },
+]
 
 const Expenses = () => (
   <Layout>
     <h1>Expenses</h1>
     <p>Welcome to expenses!</p>
+    <List component="nav">
+      {data &&
+        data.map(expense => (
+          <ListItem button key={expense.id}>
+            <ListItemText primary={expense.kind} />
+          </ListItem>
+        ))}
+    </List>
+    <Modal />
   </Layout>
 )
 
