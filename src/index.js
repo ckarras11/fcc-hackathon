10cb95a4f11af946475d6a9e20f6f6436e2adcc3
Christopher Karras <ckarras11@gmail.com>
2018-11-04 14:41:51 -0500

    Registered serviceworker

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 
-import netlifyIdentity from 'netlify-identity-widget';
+import netlifyIdentity from 'netlify-identity-widget'
 
-window.netlifyIdentity = netlifyIdentity;
+window.netlifyIdentity = netlifyIdentity
 // You must run this once before trying to interact with the widget
-netlifyIdentity.init();
+netlifyIdentity.init()
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register()
