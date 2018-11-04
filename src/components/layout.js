80bf7b00ef43eaca0742367cf53e42ab52f31c22
Christopher Karras <ckarras11@gmail.com>
2018-11-04 09:34:06 -0500

    Put images on cloudinary

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'
+import PropTypes from 'prop-types'
 
-import SideBar from './SideBar';
-import './layout.css';
+import SideBar from './SideBar'
+import '../styles/layout.css'
 
 const Layout = ({ children }) => {
-	return (
-		<SideBar>
-			<div
-				style={{
-					margin: '0 auto',
-					maxWidth: 960,
-					padding: '0px 1.0875rem 1.45rem',
-					paddingTop: 0
-				}}
-			>
-				{children}
-			</div>
-		</SideBar>
-	);
-};
+  return (
+    <SideBar>
+      <div
+        style={{
+          margin: '0 auto',
+          maxWidth: 960,
+          padding: '0px 1.0875rem 1.45rem',
+          paddingTop: 0,
+        }}
+      >
+        {children}
+      </div>
+    </SideBar>
+  )
+}
 
 Layout.propTypes = {
-	children: PropTypes.node.isRequired
-};
+  children: PropTypes.node.isRequired,
+}
 
-export default Layout;
+export default Layout
